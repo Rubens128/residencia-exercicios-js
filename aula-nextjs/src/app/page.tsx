a6e79b28235e62e91f819d573e2e0343eab231bc
Rubens de Oliveira Cristiano <rubens.cristiano@a.ucb.br>
2025-05-30 18:38:51 -0300

    feat/onChangeInput

--- aula-nextjs/src/app/page.tsx
+++ aula-nextjs/src/app/page.tsx
@@ -1,6 +1,23 @@
 "use client";
+import { use, useState } from "react";
 import styles from "./page.module.css";
+import { stringify } from "querystring";
 
 export default function Home() {
-  return <button className={styles.page}></button>;
+  
+  const [texto, setTexto] = useState("");
+
+  function changeP(event: React.ChangeEvent<HTMLInputElement>){
+      setTexto(event.target.value);
+  };
+
+  return (
+    <div className={styles.div}>
+      <h1>Input:</h1>
+      <input onChange={changeP} className={styles.input}></input>
+      <h2>Paragrafo:</h2>
+      <p className={styles.p}>{texto}</p>
+    </div>
+
+  );
 }

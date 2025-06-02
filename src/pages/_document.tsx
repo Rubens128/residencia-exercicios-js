d753f123d0508cc2857b7752e5bd8aacf669cc19
Allan Julie Fontes de Oliveira <allanzinhofontes@gmail.com>
2025-06-02 15:54:52 -0300

    feat(font-and-colors): fonts, colors and variable were configured

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,33 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt-BR">
+      {/* Fontes: Google Fonts */}
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="anonymous"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet"
+      />
+
+      {/* Fontes locais */}
+      <link
+        rel="preload"
+        href="/fonts/the-last-shuriken.ttf"
+        as="font"
+        type="font/ttf"
+        crossOrigin="anonymous"
+      />
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
